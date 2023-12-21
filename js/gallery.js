3f20a953201ad04bd60ad2e5e4a3db071251ec73
sujeong <su.jeong.kim@worksmobile.com>
2023-12-21 19:41:31 +0900

    복사 기능 추가

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 (function($) {
     "use strict";
-	
+	var varUA = navigator.userAgent.toLowerCase();
+
 	$(document).ready(function() {
 		$('#fh5co-gallery-list').magnificPopup({
 			delegate: 'a',
@@ -21,4 +22,51 @@
 		});
 	});
 
+	//계좌 클립보드 복사
+	$("div.account_no_copy").on('click', function (e) {
+		e.preventDefault();
+		var account = $(this).attr('data-account');
+		copyToClipboard(account);
+
+		console.log("asd");
+
+		if (varUA.indexOf('android') > -1) {
+			return false;
+		} else {
+			console.log("asdasd");
+			toastPopup('복사되었습니다.', 1500);
+		}
+	});
+
+
 }(jQuery));
+
+//클립보드 복사
+var copyToClipboard = function (text) {
+	var aux = document.createElement("textarea");
+	aux.value = text;
+	document.body.appendChild(aux);
+	aux.select();
+	aux.setSelectionRange(0, 9999);
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+};
+
+//팝업창 표시
+var toastPopup = function (msg, timer) {
+	var $elem = $("<p>" + msg + "</p>");
+
+	$("div.toast").html($elem).show();
+
+	$elem.slideToggle(100, function () {
+		setTimeout(function () {
+			$elem.fadeOut(function () {
+				$(this).remove();
+				$('div.toast').css('bottom', '');
+			});
+		}, timer);
+		return false;
+	});
+
+	$('div.toast').stop().animate({ 'bottom': '5%' });
+};
