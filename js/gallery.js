6a640114f7691425412d2b70d67e353608e64c2d
sujeong <su.jeong.kim@worksmobile.com>
2023-12-21 19:14:17 +0900

    갤러리 넘기기 추가

---
+++ js/gallery.js
@@ -0,0 +1,24 @@
+(function($) {
+    "use strict";
+	
+	$(document).ready(function() {
+		$('#fh5co-gallery-list').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title');
+				}
+			}
+		});
+	});
+
+}(jQuery));
